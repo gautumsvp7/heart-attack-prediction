6652f85439718c7ba78e3d505e0488bea0b9120f
Gautum Subhash <45570461+gautumsvp7@users.noreply.github.com>
2024-12-09 18:22:14 +1100

    Added patient id to framingham csv

--- framingham.py
+++ framingham.py
@@ -1,13 +1,14 @@
 '''
-run only once:
-'''
+#run only once:
+
 import pandas as pd
 
 # Load the dataset
-df = pd.read_csv("framingham.csv")
+df = pd.read_csv("./data/framingham.csv")
 
 # Add a synthetic primary key
 df.insert(0, 'patient_id', range(1, len(df) + 1))
 
 # Save the updated dataset
 df.to_csv("framingham_with_id.csv", index=False)
+'''
